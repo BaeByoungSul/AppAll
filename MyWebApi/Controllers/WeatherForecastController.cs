1b916924db2d88931d667f516d378ef16a086a1f
bsbae1 <bsbae1@kolon.com>
2023-10-19 16:03:49 +0900

    add authorize to action in controller

--- MyWebApi/Controllers/WeatherForecastController.cs
+++ MyWebApi/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyWebApi.Controllers
@@ -9,7 +10,7 @@ namespace MyWebApi.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,8 +19,58 @@ namespace MyWebApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [Authorize(Roles ="Admin,User")]
+        [HttpGet("GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [Authorize(Roles = "User")]
+        [HttpGet("GetWeatherForecast2")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("GetWeatherForecast3")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast3()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [Authorize(Roles = "Huizhou")]
+        [HttpGet("GetWeatherForecast4")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast4()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetWeatherForecast5")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast5()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
